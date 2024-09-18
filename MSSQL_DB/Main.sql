582dcca5afb231af7df95ba0c332cd1b3e20b0da
ThomasB98 <bansodthomas@gmail.com>
2024-09-18 14:51:10 +0530

    [Thomas]ADD:Employee payroll table

--- MSSQL_DB/Main.sql
+++ MSSQL_DB/Main.sql
@@ -5,3 +5,10 @@ CREATE DATABASE payroll_service;
 SELECT name 
 FROM sys.databases;
 
+CREATE TABLE employee.employee_payroll(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50) NOT NUll,
+    salary Int NOT NULL DEFAULT 10000,
+    start_date DATE NOT NULL
+);
+
